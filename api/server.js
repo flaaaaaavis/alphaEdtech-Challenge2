cc7f35ed8439c7c0a580c45a7b1026d15b258520
flavia <sflavia051@gmail.com>
2022-04-20 16:13:13 -0300

    Idk

---
+++ api/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+const userRoutes = require('./controllers/userController')
+const productRoutes = require('./controllers/productController')
+const addressRoutes = require('./controllers/addressController')
+const contactRoutes = require('./controllers/contactController')
+const storeRoutes = require('./controllers/storeController')
+const sizeRoutes = require('./controllers/sizeController')
+
+app.use(express.static('./frontend'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+app.use(userRoutes)
+app.use(productRoutes)
+app.use(addressRoutes)
+app.use(contactRoutes)
+app.use(storeRoutes)
+app.use(sizeRoutes)
+
+// app.post('/image', imageUpload.single('image'), (req, res) => { 
+//     console.log(req.file);
+//     res.json('/image api'); 
+// });
+
+app.listen(3000, () =>
+    console.log('App listening on port 3000')
+)
---
+++ frontend/scripts/users-script.js
@@ -0,0 +1,43 @@
+const apiURL = 'http://localhost:3000/';
+const userId = [12]
+
+function readUsers(){
+    fetch(apiURL+'readAllUsers', {
+        method: "get",
+        headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+        response.json().then(users => {
+            const listUsuarios = document.getElementById("lista-usuarios")
+            users.map(user => {
+                const li = document.createElement("li")
+                li.innerHTML = `${user.name} - ${user.cpf}`
+                listUsuarios.appendChild(li)
+            })
+        })
+    });
+}
+
+function saveUser(){
+    const name = document.getElementById("nome")
+    const cpf = document.getElementById("cpf")
+    const username = document.getElementById("username")
+    const senha = document.getElementById("senha")
+    fetch(apiURL+'createUser', {
+        method: "post",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: name.value,
+            cpf: cpf.value,
+            user: username.value,
+            pass: senha.value
+        })
+    }).then(response => {
+        if (response.status === 201) {
+            name.value = ""
+            cpf.value = ""
+            username.value = ""
+            senha.value = ""
+            alert("Usuário salvo com sucesso.")
+        }
+    });
+}
